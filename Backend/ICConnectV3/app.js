12f2f3ed93f15fe8e0525501b694150d53058df5
YaHuiLiang <yahuiliang@ICs-MacBook-Pro.local>
2016-12-16 18:43:18 +0800

    init test project

---
+++ Backend/ICConnectV3/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json('error').end();
+});
+
+module.exports = app;
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# RRN
-React+Redux+Nodejs Restful API
+# RRN(React+Redux+Nodejs)
+This is a Web Application Solution basic on React+Redux+Nodejs
+
+# Author
+ryouaki(46517115@qq.com)
+
+# License
+MIT
